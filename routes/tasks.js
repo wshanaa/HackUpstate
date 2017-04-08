9df9aca25de9c50fb5abb3d15e870670ea60f050
Brian Dorsey <bdor528@gmail.com>
2017-04-08 19:13:31 -0400

    Getting started
    The server before mongodb is connected

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
 
+var db = mongodb('', ['collection']);
+
+//Get all tasks
 router.get('/tasks', function(req, res, next){
 	res.send('TASK API');
+	db.tasks.find(function(err, tasks){
+		if(err){
+			res.send(err);
+		}
+		res.json(tasks);
+	});
+});
 
+//Get single tasks
+router.get('/task/:id', function(req, res, next){
+	res.send('TASK API');
+	db.tasks.findOne({_id: mongodb.ObjectId(req.params.id)}, function(err, task){
+		if(err){
+			res.send(err);
+		}
+		res.json(task);
+	});
 });
 
 
