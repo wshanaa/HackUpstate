9db51001c514d52347bd888e0bb597b0694fe289
Brian Dorsey <bdor528@gmail.com>
2017-04-08 22:05:15 -0400

    File name changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var tasks = require('./routes/tasks');
+var tasks = require('./routes/endpoints');
 
 var port = 8000;
 
